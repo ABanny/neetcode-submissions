500a7fab32fd303740a153f4b3716e32d6e26328
ABanny <62797550+ABanny@users.noreply.github.com>
2026-05-11 20:02:44 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            chars_s = []
+            chars_t = []
+            for a in s:
+                chars_s.append(a)
+            for b in t:
+                chars_t.append(b)
+
+            chars_s.sort()
+            chars_t.sort()
+
+            for n in range(len(chars_s)):
+                if chars_s[n] == chars_t[n]:
+                    anagram = True
+                else:
+                    anagram = False
+                    break
+
+            if anagram:
+                return True
+            else:
+                return False
+
+        else:
+            return False
